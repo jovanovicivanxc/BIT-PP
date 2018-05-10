4722b23b27d9fd9b0a2431b988f1a76aa1e51f73
BIT student <classroom@bgit.rs>
2018-05-10 17:47:36 +0200

    w4-d4

---
+++ W4_airport/airport.js
@@ -0,0 +1,158 @@
+'use strict';
+
+(function () {
+
+    function Person(name, surname) {
+        this.name = name;
+        this.surname = surname;
+        this.getData = function (params) {
+            return name + " " + surname;
+        }
+    }
+
+    function Seat(number, category) {
+
+
+
+        this.number = (function () {
+            var num;
+            if (number == undefined) {
+                num = parseInt(Math.random() * 100);
+            }
+            else {
+                num = number;
+            }
+            return num;
+        })();
+
+        this.category = (function () {
+            var cat = "E";
+            if (category == "B") {
+                cat = "B"
+            }
+            else {
+                cat = "E"
+            }
+            return cat;
+        })();
+
+        this.getData = function () {
+            return this.number + ", " + this.category;
+        }
+    }
+
+
+
+    function Passenger(person, seat) {
+        this.person = person;
+        this.seat = seat;
+        this.getData = function () {
+            return seat.number + ', ' + seat.category + ', ' + person.getData();
+        }
+    }
+
+    function Flight(relation, date, list) {
+        this.relation = relation;
+        this.date = new Date(date).toDateString();
+        this.list = [];
+        this.addPassenger = function (passenger) {
+            this.list.push(passenger);
+            return this.list;
+        }
+        this.getData = function () {
+            var text = "";
+            for (var i = 0; i < this.list.length; i++) {
+                text += "\n" + this.list[i].getData();
+            }
+            return this.date + ", " + this.relation + ", " + text;
+        }
+    }
+
+    function Airport(name, listofFlights) {
+        this.name = name;
+        this.listofFlights = [];
+        this.addFlight = function (flight) {
+            this.listofFlights.push(flight);
+            return this.listofFlights;
+        }
+    }
+
+
+    var pera = new Person("Petar", "Petrovic");
+    var zika = new Person("Zivorad", "Zivic");
+
+    console.log(pera.getData());
+
+    var seat1 = new Seat();
+    var seat2 = new Seat();
+
+    console.log(seat1.category);
+    console.log(seat1.number);
+    console.log(seat1.getData());
+
+    var pass1 = new Passenger(pera, seat1);
+    var pass2 = new Passenger(zika, seat2);
+
+    console.log(pass1.getData());
+
+    var let1 = new Flight("Belgrade-Instabul", "12/10/2018", );
+    let1.addPassenger(pass1);
+    let1.addPassenger(pass2);
+
+    console.log(let1.list);
+
+    var aerodrom = new Airport("Nikola Tesla", );
+    aerodrom.addFlight(let1);
+    console.log(aerodrom.listofFlights);
+
+    console.log(let1.getData());
+
+
+
+
+
+    // function createFlight(relation, date) {
+    //     return new Flight(relation, date);
+    // }
+
+    // var newYork = createFlight('Belgrade-NewYork', '25/10/2018');
+    // var barcelona = createFlight('Barcelona-Belgrade', '11/11/2018');
+
+    // function createPassenger(seat, category) {
+    //     return new Passenger(seat, category);
+    // }
+
+    // var john = createPassenger("1", 'B');
+    // var cersei = createPassenger("2", 'B');
+    // var daenerys = createPassenger("14", );
+    // var tyrion = createPassenger();
+
+    // newYork.addPassenger(john);
+    // newYork.addPassenger(cersei);
+
+    // barcelona.addPassenger(daenerys);
+    // barcelona.addPassenger(tyrion);
+
+    // var aerodrom = new Airport("Nikola Tesla", );
+    // aerodrom.addFlight(newYork);
+    // aerodrom.addFlight(barcelona);
+
+    // console.log(barcelona.getData());
+
+
+
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
